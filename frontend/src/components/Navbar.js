b9caddf0ce8be333139ae54c18208f7b6b495667
Sai Likhith Kanuparthi <slk522@nyu.edu>
2023-07-23 04:35:02 -0500

    updated dashboard

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,7 +1,7 @@
 // frontend/src/components/Navbar.js
 
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import styled from "styled-components";
 
 const StyledNavbar = styled.nav`
@@ -27,14 +27,31 @@ const StyledLink = styled(Link)`
 `;
 
 function Navbar({ authenticated, handleLogout }) {
+  const location = useLocation();
+  const isLoginOrRegisterPage = ["/login", "/register"].includes(
+    location.pathname
+  );
+
   return (
     <StyledNavbar>
-      <StyledLink to="/">Go To Home</StyledLink>
+      {!isLoginOrRegisterPage && <StyledLink to="/">Home</StyledLink>}
       {authenticated ? (
-        <StyledLink to="/logout" onClick={handleLogout}>
-          Logout
-        </StyledLink>
-      ) : null}
+        <>
+          <StyledLink to="/dashboard">Dashboard</StyledLink>
+          <StyledLink to="/quiz">Quiz</StyledLink>
+          <StyledLink to="/result">Result</StyledLink>
+          <StyledLink to="/logout" onClick={handleLogout}>
+            Logout
+          </StyledLink>
+        </>
+      ) : (
+        <>
+          {!isLoginOrRegisterPage && <StyledLink to="/login">Login</StyledLink>}
+          {!isLoginOrRegisterPage && (
+            <StyledLink to="/register">Register</StyledLink>
+          )}
+        </>
+      )}
     </StyledNavbar>
   );
 }
--- frontend/src/components/auth/Login.js
+++ frontend/src/components/auth/Login.js
@@ -2,12 +2,52 @@ import React, { useState } from "react";
 import axios from "axios";
 import { Link, useNavigate } from "react-router-dom";
 import styled from "styled-components";
+import quizImage from "../../assets/quiz-time.webp"; // <-- Add this
+
+const StyledButton = styled(Link)`
+  display: inline-block;
+  background-color: red;
+  color: white;
+  padding: 0.5rem 1rem;
+  margin: 0.5rem;
+  border-radius: 5px;
+  text-decoration: none;
+  transition: background-color 0.3s ease;
+
+  &:hover {
+    background-color: #b30000;
+  }
+`;
+
+const ButtonsContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  width: 200px;
+  position: absolute;
+  right: 2rem;
+  top: 2rem;
+`;
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: calc(100vh - 50px);
+  position: relative;
+  background-color: #000;
+`;
+
+const QuizImage = styled.img`
+  max-width: 200%;
+  height: auto;
+  margin-right: 10%;
+`;
 
 const Form = styled.form`
-  background-color: #fff;
-  padding: 20px;
-  border-radius: 5px;
-  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
+  padding: 40px;
+  width: 400px;
+  background-color: transparent;
 `;
 
 const InputText = styled.input.attrs((props) => ({
@@ -15,8 +55,8 @@ const InputText = styled.input.attrs((props) => ({
 }))`
   display: block;
   width: 100%;
-  padding: 10px;
-  margin-bottom: 10px;
+  padding: 15px;
+  margin-bottom: 20px;
   border: 1px solid #007aff;
   border-radius: 5px;
 `;
@@ -39,19 +79,19 @@ const InputSubmit = styled.input.attrs((props) => ({
   color: #fff;
   padding: 10px;
   border: none;
-  border-radius: 50px;
+  border-radius: 5px;
   cursor: pointer;
   transition: background-color 0.3s ease;
 
   &:hover {
-    background-color: darkred;
+    background-color: #b30000;
   }
 `;
 
 function Login({ handleLogin }) {
+  const navigate = useNavigate();
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -71,23 +111,29 @@ function Login({ handleLogin }) {
   };
 
   return (
-    <Form onSubmit={handleSubmit}>
-      <InputText
-        placeholder="Username"
-        onChange={(e) => setUsername(e.target.value)}
-      />
-      <InputPassword
-        placeholder="Password"
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <InputSubmit value="Login" />
-      <p>
-        New user? <Link to="/register">Register</Link>
-      </p>
-      <p>
-        <Link to="/reset-password">Forgot Password?</Link>
-      </p>
-    </Form>
+    <Container>
+      <QuizImage src={quizImage} alt="Quiz" />
+      <Form onSubmit={handleSubmit}>
+        <InputText
+          placeholder="Username"
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <InputPassword
+          placeholder="Password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <InputSubmit value="Login" />
+        <p>
+          New user? <StyledButton to="/register">Register</StyledButton>
+        </p>
+        <p>
+          <Link to="/reset-password">Forgot Password?</Link>
+        </p>
+      </Form>
+      <ButtonsContainer>
+        <StyledButton to="/">Go to Home</StyledButton>
+      </ButtonsContainer>
+    </Container>
   );
 }
 
